1838a261eb95514c12884053c0218bf4e83aca8a
Maryanne24 <maryanne.g@escolar.ifrn.edu.br>
2026-07-13 01:55:29 +0000

    iniciando a estrutura do projeto; blog,py , schema.sql, e a pasta blog

--- catalogo-skincare-app/flaskr/blog.py
+++ catalogo-skincare-app/flaskr/blog.py
@@ -12,7 +12,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, produto, preco, comentario, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -22,21 +22,22 @@ def index():
 @login_required
 def create():
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        produto = request.form['produto']
+        preco = request.form['preco']
+        comentario = request.form['comentario']
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not produto:
+            error = 'O nome do produto é obrigatorio.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
+                'INSERT INTO post (produto, preco, comentario, author_id)'
                 ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                (produto, preco, comentario, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -45,7 +46,7 @@ def create():
 
     def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, produto, preco, comentario, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -64,21 +65,22 @@ def update(id):
     post = get_post(id)
 
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        produto = request.form['produto']
+        preco = request.form['preco']
+        comentario = request.form['comentario']
         error = None
 
         if not title:
-            error = 'Title is required.'
+            error = 'O nome do produto é obrigatorio.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'UPDATE post SET title = ?, body = ?'
+                'UPDATE post SET produto = ?, preco = ?' , comentario = ?'
                 ' WHERE id = ?',
-                (title, body, id)
+               (produto , preco,comentario , id)
             )
             db.commit()
             return redirect(url_for('blog.index'))
--- catalogo-skincare-app/flaskr/schema.sql
+++ catalogo-skincare-app/flaskr/schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE post (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   author_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title TEXT NOT NULL,
-  body TEXT NOT NULL,
+  produto TEXT NOT NULL,
+  preco TEXT NOT NULL,
+  comentario TEXT,
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
